f3e294c46e10699d0e020e5149004f632c15513b
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-01-16 19:00:40 -0800

    server.js updated

--- server.js
+++ server.js
@@ -12,58 +12,33 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-var characters = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+app.get("/api/tables", function(req, res) {
+  return res.json(table);
 });
 
 // Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
+app.get("/api/tables/:table", function(req, res) {
+  var chosen = req.params.table;
 
   console.log(chosen);
 
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+  for (var i = 0; i < tables.length; i++) {
+    if (chosen === tables[i].routeName) {
+      return res.json(tables[i]);
     }
   }
 
@@ -71,20 +46,60 @@ app.get("/api/characters/:character", function(req, res) {
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCharacter);
-
-  characters.push(newCharacter);
-
-  res.json(newCharacter);
+app.post("/api/tables", function(req, res) {
+  //<!-- BELOW CODE IS CRITICAL. IT HANDLES HOW FORM DATA IS SENT TO OUR SERVER -->
+  
+  
+    // In this code below we create the Front-end Javascript which "POSTS" our form data to our express server.
+    // In essence, when the user hits submit, jQuery grabs all of the fields then sends a post request to our api
+    // Our api recognizes the route (/api/tables)... and then runs the associated code (found in api-routes.js).
+    // In this case the associated code "saves" the data to the table-data.js file or waitinglist-data.js file
+  
+    $(".submit").on("click", function(){
+  
+      // Here we grab the form elements
+      var newReservation = {
+        customerName: $('#reserve_name').val().trim(),
+        phoneNumber: $('#reserve_phone').val().trim(),
+        customerEmail: $('#reserve_email').val().trim(),
+        customerID: $('#reserve_uniqueID').val().trim()
+      };
+  
+      console.log(newReservation);
+  
+      // This line is the magic. It's very similar to the standard ajax function we used.
+      // Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
+      // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
+      // depending on if a tables is available or not.
+  
+      // Here we get the location of the root page.
+      // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+      var currentURL = window.location.origin;
+  
+        $.post(currentURL + "/api/tables", newReservation,
+        function(data){
+  
+          // If a table is available... tell user they are booked.
+          if(data == true){
+            alert("Yay! You are officially booked!")
+          }
+  
+          // If a table is available... tell user they on the waiting list.
+          if(data == false){
+            alert("Sorry you are on the wait list")
+          }
+  
+          // Clear the form when submitting
+          $('#reserve_name').val("");
+          $('#reserve_phone').val("");
+          $('#reserve_email').val("");
+          $('#reserve_uniqueID').val("");
+  
+        });
+  
+  return false;
+  });
+  
 });
 
 // Starts the server to begin listening
