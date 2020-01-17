2afe5315e38eb7b9f6c42a3792f2d2e262692532
Brad Johnston <twopheat@gmail.com>
2020-01-16 21:02:40 -0800

    reserve changes

---
+++ api/tables/table-data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        name: "John Smith", 
+        email: "johnsmith@email.com", 
+        phoneNumber: '808 888 8080',
+        seats: 2
+      
+    },
+    {
+        name: "Bob Ross", 
+        email: "bobross@gmail.com", 
+        phoneNumber: '808 808 8080',
+        seats: 4
+        
+    },
+    {
+        name: "Kanye West", 
+        email: "yeezus@gmail.com", 
+        phoneNumber: '808 898 8080',
+        seats: 6
+        
+    }
+];
